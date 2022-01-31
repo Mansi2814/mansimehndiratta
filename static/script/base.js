b233dca6a8de886a2a1f54aae31879225fca1561
Mansi Mehndiratta <mehndiratta5369@gmail.com>
2022-02-01 01:11:04 +0530

    navbar edited

--- static/script/base.js
+++ static/script/base.js
@@ -1,9 +1,6 @@
 let about = document.getElementById("about-button")
 let greetings = document.getElementById("greetings")
-console.log(about)
-console.log(greetings)
 about.addEventListener('click', function() {
-    console.log("hi")
     greetings.classList.add("rotate-animation");
     setTimeout(()=>{
         greetings.classList.remove("rotate-animation");
